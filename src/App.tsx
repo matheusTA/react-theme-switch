7002be8f7e0bffedce21228519f1c4650513c6c1
Matheus Antonino <matrajano12@gmail.com>
2020-06-10 00:10:54 -0300

    Implement switch theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import usePersistedState from "./hooks/usePersistedState";
+
+import GlobalStyle from "./styles/global";
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  const [theme, setTheme] = usePersistedState<DefaultTheme>("theme", light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
