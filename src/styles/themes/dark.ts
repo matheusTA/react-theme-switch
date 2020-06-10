3c933418b9ee0e9867ce6bb6e661294bd626409c
Matheus Antonino <matrajano12@gmail.com>
2020-06-10 00:10:00 -0300

    Create theme styles

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: "dark",
+
+  colors: {
+    primary: "#333",
+    secundary: "#c62e65",
+
+    background: "#222",
+    text: "#fff",
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: "light",
+
+  colors: {
+    primary: "#c62e65",
+    secundary: "#d63af9",
+
+    background: "#f5f5f5",
+    text: "#333",
+  },
+};
